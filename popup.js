ca87ce0eb15a776c34db9d2554f728724f889d4d
Dong Zhao <dzhao@redhat.com>
2019-05-26 23:47:02 +0800

    use a real hostname for REST API service

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ let getdata = document.getElementById('getdata');
 getdata.onclick = function() {
   console.log("btn get data")
   $.ajax({
-        url: "http://localhost:3000/api/car/5"
+        url: "http://zhaodong.usersys.redhat.com:3000/api/car/1"
   }).then(function(data) {
     console.log(data);
     $('#editdialog').html(data.id+', '+ data.maker+', '+ data.model+', '+ data.driver + ', ' + data.year);
